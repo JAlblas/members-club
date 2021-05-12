214a1f18aef6f75c2c46332802e48ece74c0971f
Jasper Alblas <just_jasper@hotmail.com>
2021-05-12 17:00:39 +0200

    Added ability to register users '

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +14,25 @@ router.get('/register', function(req, res, next) {
 });
 
 router.post('/register', function(req, res, next) {
-  res.send('Form posted');
+  let name = req.body.name;
+  let email = req.body.email;
+  let password = req.body.password;
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+      if (err) {
+          return next(err);
+      }
+      const user = new User({
+          name: name,
+          email: email,
+          password: hashedPassword,
+          member: false
+        }).save(err => {
+          if (err) { 
+            return next(err);
+          };
+          res.redirect("/");
+        });
+    });
 });
 
 module.exports = router;

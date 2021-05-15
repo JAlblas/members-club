270767d9a258eec20cde4f139af40ff614e1abec
Jasper Alblas <just_jasper@hotmail.com>
2021-05-15 23:04:00 +0200

    part of authentication added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 
 var User = require('../models/user');
 var userController = require('../controllers/userController');
@@ -16,34 +18,33 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register profile' });
 });
 
-router.post('/register', function(req, res, next) {
-  check('email')
-  .isEmail(),
-  check('password').exists(),
-  check(
-    'passwordRepeat',
-    'passwordConfirmation field must have the same value as the password field',
-  )
+router.post(
+  '/register',   
+  body('email').isEmail().normalizeEmail(),
+  body('password').exists().isLength({ min: 5 }),
+  function(req, res, next) {
 
-  let name = req.body.name;
-  let email = req.body.email;
-  let password = req.body.password;
-  bcrypt.hash(password, 10, (err, hashedPassword) => {
-      if (err) {
-          return next(err);
-      }
-      const user = new User({
-          name: name,
-          email: email,
-          password: hashedPassword,
-          member: "Member"
-        }).save(err => {
-          if (err) { 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) {
             return next(err);
-          };
-          res.redirect("/");
-        });
-    });
+        }
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+            member: "Member"
+          }).save(err => {
+            if (err) { 
+              return next(err);
+            };
+            res.redirect("/");
+          });
+      });
 });
 
 module.exports = router;
